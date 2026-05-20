5e49b739b743d29306038fd48a016cf55889989c
placideniyo01-gif <placideniyo01@gmail.com>
2026-05-20 18:06:26 +0200

    fix login urls

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.login_view, name='login'),
-
+    path("login/", views.login_view, name="login"),
     path('register/', views.register_view, name='register'),
 
     path('dashboard/', views.dashboard, name='dashboard'),
