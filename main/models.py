e3d9ff1993044d2a4f7ec2f996b96111edd00540
placideniyo01-gif <placideniyo01@gmail.com>
2026-05-20 16:12:51 +0200

    add support chat model

--- main/models.py
+++ main/models.py
@@ -252,6 +252,38 @@ class ReferralBonus(models.Model):
         auto_now_add=True
     )
 
+# =========================
+# SUPPORT CHAT
+# =========================
+class SupportMessage(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    sender = models.CharField(
+        max_length=20,
+        choices=(
+            ("user", "user"),
+            ("admin", "admin"),
+        )
+    )
+
+    message = models.TextField()
+
+    is_read = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return f"{self.user} - {self.sender}"
+        
     @property
     def is_active(self):
 
